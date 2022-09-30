d72313b6c11d5c3464ae96b9d7f3fe470b349227
聂金浩 <>
2022-09-30 11:08:22 +0800

    创建工程

---
+++ JHBaseApplication/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  JHBaseApplication
+//
+//  Created by 聂金浩 on 2022/9/30.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ JHBaseApplication/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  JHBaseApplication
+//
+//  Created by 聂金浩 on 2022/9/30.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
